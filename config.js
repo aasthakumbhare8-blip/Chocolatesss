06fbc81eb428e835d0bbec926c48c455310c3324
aasthakumbhare8-blip <aasthakumbhare8@gmail.com>
2026-02-06 23:21:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
+            text: "Will you be my Valentine on February 14th, 2025? ", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -42,16 +42,16 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "offcourse You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "offcourse You love me heheh?? 😚😚😚",  // Shows when they go past 5000%
+        high: "To infinity and beyond! 💌💌",              // Shows when they go past 1000%
+        normal: "And beyond! 💋"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: " I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        title: " I'm so happy for having you in my life 🥹💌😘😚",
+        message: "lots of love and kisses for you  😚💌💌💌",
+        emojis: "💋😘💋😘💋😘💋😘💋"  // These will bounce around
     },
 
     // Color scheme for the website
